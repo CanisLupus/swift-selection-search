4dbbba34bd730f274008c332db5ed74939bf0a9c
Daniel Lobo <feloxolofoxifalitu@gmail.com>
2018-12-25 12:41:08 +0000

    added searchTerms support for slicing, regex replacements and functions;

--- src/swift-selection-search.ts
+++ src/swift-selection-search.ts
@@ -893,7 +893,7 @@ function getSearchQuery(engine: SearchEngine_Custom, searchText: string, url: UR
 {
 	// replace newlines with spaces
 	searchText = searchText.trim().replace("\r\n", " ").replace("\n", " ");
-	let query = getFilteredSearchUrl(engine.searchUrl, searchText);
+	let query = getFilteredSearchUrl(engine.searchUrl, searchText, true);
 
 	// use regex with "g" flag to match all occurences, "i" ignores case
 	query = query.replace(/\{hash\}/gi, url.hash);
@@ -965,7 +965,140 @@ function getCurrentTab(callback)
 	);
 }
 
-function getFilteredSearchUrl(url: string, text: string): string
+abstract class SearchTermsModification
+{
+	abstract apply(text: string): string;
+}
+
+class SearchTermsSlice extends SearchTermsModification
+{
+	constructor(
+		public startIndex: number,
+		public endIndex: number)
+	{
+		super();
+	}
+
+	apply(text: string): string
+	{
+		let endIndex = this.endIndex;
+		if (endIndex === null) {
+			endIndex = text.length;
+		} else if (endIndex < 0) {
+			endIndex += text.length;
+		}
+
+		let startIndex = this.startIndex;
+		if (startIndex === null) {
+			startIndex = 0;
+		} else if (startIndex < 0) {
+			startIndex += text.length;
+			if (endIndex === 0) {
+				endIndex = text.length;
+			}
+		}
+
+		try {
+			return text.substring(startIndex, endIndex);
+		} catch {
+			return text;
+		}
+	}
+}
+
+class SearchTermsReplacement extends SearchTermsModification
+{
+	constructor(
+		public source: string,
+		public target: string)
+	{
+		super();
+	}
+
+	apply(text: string): string
+	{
+		return text.split(this.source).join(this.target);	// replace all occurrences
+	}
+}
+
+class SearchTermsRegexReplacement extends SearchTermsModification
+{
+	constructor(
+		public source: string,
+		public flags: string,
+		public target: string)
+	{
+		super();
+	}
+
+	apply(text: string): string
+	{
+		try {
+			let regex = new RegExp(this.source, this.flags);
+			return text.replace(regex, this.target);	// replace all occurrences
+		} catch {
+			return text;
+		}
+	}
+}
+
+class SearchTermsRegexMatch extends SearchTermsModification
+{
+	constructor(
+		public source: string,
+		public flags: string)
+	{
+		super();
+	}
+
+	apply(text: string): string
+	{
+		try {
+			let regex = new RegExp(this.source, this.flags);
+			let match = text.match(regex)
+			return match !== null ? match.join("") : text;
+		} catch {
+			return text;
+		}
+	}
+}
+
+class SearchTermsFunction extends SearchTermsModification
+{
+	constructor(public functionName: string)
+	{
+		super();
+	}
+
+	apply(text: string): string
+	{
+		let name = this.functionName.toLowerCase();
+		switch (name)
+		{
+			case "lowercase": return text.toLowerCase();
+			case "uppercase": return text.toUpperCase();
+			// case "alphanumeric": return text.replace(/[^0-9a-z]/gi, '');	// "g" flag matches all occurences, "i" ignores case
+			default: return text;
+		}
+	}
+}
+
+class SearchTermsModifications
+{
+	constructor(
+		public modifications: SearchTermsModification[],
+		public searchTermsStartIndex: number,
+		public searchTermsEndIndex: number)
+	{
+	}
+
+	static createDefault(): SearchTermsModifications
+	{
+		return new SearchTermsModifications([], -1, -1);
+	}
+}
+
+function getFilteredSearchUrl(url: string, text: string, encode: boolean): string
 {
 	text = text.trim();
 
@@ -974,111 +1107,242 @@ function getFilteredSearchUrl(url: string, text: string): string
 
 	while (true)
 	{
-		let [replacements, startIndex, endIndex] = getSearchTermsReplacements(url, searchIndex);
+		let modifications = getSearchTermsReplacements(url, searchIndex);
 
-		if (endIndex == -1) {
+		if (modifications.searchTermsEndIndex == -1) {
 			break;
 		}
 
-		queryParts.push(url.substring(searchIndex, startIndex));
-		// encode chars after replacement
-		queryParts.push(encodeURIComponent(replace(text, replacements)));
-		searchIndex = endIndex;
+		queryParts.push(url.substring(searchIndex, modifications.searchTermsStartIndex));
+		if (encode) {
+			// encode chars after replacement
+			queryParts.push(encodeURIComponent(replace(text, modifications.modifications)));
+		} else {
+			queryParts.push(replace(text, modifications.modifications));
+		}
+		searchIndex = modifications.searchTermsEndIndex;
 	}
 
 	queryParts.push(url.substring(searchIndex));
 	return queryParts.join("");
 }
 
-enum SearchTermsReplacementState {
-	EXPECT_REPLACE_PAIR_OR_END,
-	EXPECT_REPLACE_SOURCE,
-	EXPECT_REPLACE_RESULT,
+enum SearchTermsParserState
+{
+	EXPECTING_MODIFICATION_OR_END,	// expecting a {}, (), or [] modification block (or the end of searchTerms)
+	IN_REPLACE,						// inside a {} replacement block
+	IN_REPLACE_REGEX,				// inside a {/ /|} block, between the / /
+	IN_REPLACE_REGEX_FLAGS,			// inside a {/ /|} block, after the / /
+	IN_REPLACE_SOURCE,				// inside a {|} replacement block, before the |, and we know it's not a regex
+	IN_REPLACE_TARGET,				// inside a {|} replacement block, after the |
+	IN_FUNCTION,					// inside a () function block
+	IN_RANGE_START,					// inside a [] slice block
+	IN_RANGE_END,					// inside a [:] slice block, after the :
 }
 
-function getSearchTermsReplacements(url: string, startIndexForIndexOf: number): [Array<[string, string]>, number, number]
+function getSearchTermsReplacements(url: string, startIndexForIndexOf: number): SearchTermsModifications
 {
-	let startString: string = "{searchTerms";
+	const startString: string = "{searchTerms";
 	let startIndex: number = url.indexOf(startString, startIndexForIndexOf);
 	// if searchTerms not found, quit
 	if (startIndex === -1) {
-		return [[], -1, -1];
+		return SearchTermsModifications.createDefault();
 	}
+
+	let modifications = SearchTermsModifications.createDefault();
 
 	let index: number = startIndex + startString.length;
 	// if searchTerms ends immediately with no replacements, quit
 	if (url[index] == "}") {
-		return [[], startIndex, index+1];
+		modifications.searchTermsStartIndex = startIndex;
+		modifications.searchTermsEndIndex = index + 1;
+		return modifications;
 	}
 
-	let state: SearchTermsReplacementState = SearchTermsReplacementState.EXPECT_REPLACE_PAIR_OR_END;
+	let state: SearchTermsParserState = SearchTermsParserState.EXPECTING_MODIFICATION_OR_END;
+
 	let replacementSource: string;
-	let replacementResult: string;
-	let replacements: Array<[string, string]> = [];
+	let replacementTarget: string;
 	let isEscaped: boolean = false;
 
-	while (index < url.length)
+	let regexSource: string;
+	let regexFlags: string;
+
+	let functionName: string;
+
+	let rangeStartIndexString: string;
+	let rangeEndIndexString: string;
+
+	for (; index < url.length; index++)
 	{
 		// get char
 		let c: string = url[index];
 
-		if (!isEscaped && c === "\\") {
-			isEscaped = true;
-			index++;
-			continue;
-		}
-
 		switch (state)
 		{
-			case SearchTermsReplacementState.EXPECT_REPLACE_PAIR_OR_END:
-				if (c === "{") {
-					state = SearchTermsReplacementState.EXPECT_REPLACE_SOURCE;
+			case SearchTermsParserState.EXPECTING_MODIFICATION_OR_END: {
+				if (c === "}") {
+					modifications.searchTermsStartIndex = startIndex;
+					modifications.searchTermsEndIndex = index + 1;
+					return modifications;
+				} else if (c === "{") {
+					state = SearchTermsParserState.IN_REPLACE;
 					replacementSource = "";
-				} else if (c === "}") {
-					return [replacements, startIndex, index+1];
+				} else if (c === "[") {
+					state = SearchTermsParserState.IN_RANGE_START;
+					rangeStartIndexString = "";
+				} else if (c === "(") {
+					state = SearchTermsParserState.IN_FUNCTION;
+					functionName = "";
 				} else if (c !== " ") {
-					return [[], -1, -1];
+					return SearchTermsModifications.createDefault();
 				}
 				break;
+			}
 
-			case SearchTermsReplacementState.EXPECT_REPLACE_SOURCE:
+			case SearchTermsParserState.IN_REPLACE: {
+				if (index < url.length-2 && c === "r" && url[index+1] === "e" && url[index+2] === "/") {
+					state = SearchTermsParserState.IN_REPLACE_REGEX;
+					regexSource = "";
+					index += 2;
+				} else {
+					state = SearchTermsParserState.IN_REPLACE_SOURCE;
+					index--;
+				}
+				break;
+			}
+
+			case SearchTermsParserState.IN_REPLACE_REGEX: {
+				if (c === "/") {
+					state = SearchTermsParserState.IN_REPLACE_REGEX_FLAGS;
+					regexFlags = "";
+				} else {
+					regexSource += c;
+				}
+
+				if (c === "\\" && index < url.length-1 && url[index+1] === "/") {
+					regexSource += "/";
+					index += 1;
+				}
+				break;
+			}
+
+			case SearchTermsParserState.IN_REPLACE_REGEX_FLAGS: {
+				if (c === "|") {
+					state = SearchTermsParserState.IN_REPLACE_TARGET;
+					replacementTarget = "";
+				} else if (c === "{") {
+					return SearchTermsModifications.createDefault();
+				} else if (c === "}") {
+					modifications.modifications.push(new SearchTermsRegexMatch(regexSource, regexFlags));
+					state = SearchTermsParserState.EXPECTING_MODIFICATION_OR_END;
+				} else {
+					regexFlags += c;
+				}
+				break;
+			}
+
+			case SearchTermsParserState.IN_REPLACE_SOURCE: {
+				if (!isEscaped && c === "\\") {
+					isEscaped = true;
+					continue;
+				}
+
 				if (!isEscaped && c === "|") {
-					state = SearchTermsReplacementState.EXPECT_REPLACE_RESULT;
-					replacementResult = "";
-				} else if (!isEscaped && (c === "{" || c === "}")) {
-					return [[], -1, -1];
+					state = SearchTermsParserState.IN_REPLACE_TARGET;
+					replacementTarget = "";
+				} else if (!isEscaped && c === "{") {
+					return SearchTermsModifications.createDefault();
+				} else if (!isEscaped && c === "}") {
+					return SearchTermsModifications.createDefault();
 				} else {
 					replacementSource += c;
 				}
 				break;
+			}
 
-			case SearchTermsReplacementState.EXPECT_REPLACE_RESULT:
+			case SearchTermsParserState.IN_REPLACE_TARGET: {
+				if (!isEscaped && c === "\\") {
+					isEscaped = true;
+					continue;
+				}
+
 				if (!isEscaped && c === "}") {
-					state = SearchTermsReplacementState.EXPECT_REPLACE_PAIR_OR_END;
-					replacements.push([replacementSource, replacementResult]);
+					if (regexSource && regexSource.length > 0) {
+						modifications.modifications.push(new SearchTermsRegexReplacement(regexSource, regexFlags, replacementTarget));
+					} else {
+						modifications.modifications.push(new SearchTermsReplacement(replacementSource, replacementTarget));
+					}
+					state = SearchTermsParserState.EXPECTING_MODIFICATION_OR_END;
 				} else if (!isEscaped && (c === "|" || c === "}")) {
-					return [[], -1, -1];
+					return SearchTermsModifications.createDefault();
 				} else {
-					replacementResult += c;
+					replacementTarget += c;
 				}
 				break;
+			}
+
+			case SearchTermsParserState.IN_FUNCTION: {
+				if (c === ")") {
+					modifications.modifications.push(new SearchTermsFunction(functionName));
+					state = SearchTermsParserState.EXPECTING_MODIFICATION_OR_END;
+				} else {
+					functionName += c;
+				}
+				break;
+			}
+
+			case SearchTermsParserState.IN_RANGE_START: {
+				if (c === "]") {
+					let rangeStartIndex = rangeStartIndexString.length > 0 ? Number(rangeStartIndexString) : NaN;
+					if (isNaN(rangeStartIndex)) {
+						return SearchTermsModifications.createDefault();
+					} else {
+						modifications.modifications.push(new SearchTermsSlice(rangeStartIndex, rangeStartIndex + 1));
+					}
+
+					state = SearchTermsParserState.EXPECTING_MODIFICATION_OR_END;
+				} else if (c === ":") {
+					state = SearchTermsParserState.IN_RANGE_END;
+					rangeEndIndexString = "";
+				} else {
+					rangeStartIndexString += c;
+				}
+				break;
+			}
+
+			case SearchTermsParserState.IN_RANGE_END: {
+				if (c === "]") {
+					state = SearchTermsParserState.EXPECTING_MODIFICATION_OR_END;
+
+					let rangeStartIndex = rangeStartIndexString.length > 0 ? Number(rangeStartIndexString) : null;
+					let rangeEndIndex = rangeEndIndexString.length > 0 ? Number(rangeEndIndexString) : null;
+
+					if (rangeStartIndex === NaN || rangeEndIndex === NaN) {
+						return SearchTermsModifications.createDefault();
+					} else {
+						modifications.modifications.push(new SearchTermsSlice(rangeStartIndex, rangeEndIndex));
+					}
+				} else {
+					rangeEndIndexString += c;
+				}
+				break;
+			}
 		}
 
 		if (isEscaped) {
 			isEscaped = false;
 		}
-
-		index++;
 	}
 
-	return [[], -1, -1];
+	return SearchTermsModifications.createDefault();
 }
 
-function replace(text: string, replacements: Array<[string, string]>): string
+function replace(text: string, modifications: SearchTermsModification[]): string
 {
-	for (let i = 0; i < replacements.length; i++) {
-		let [replacementSource, replacementResult] = replacements[i];
-		text = text.split(replacementSource).join(replacementResult);	// replace all occurrences
+	for (let i = 0; i < modifications.length; i++) {
+		let modification: SearchTermsModification = modifications[i];
+		text = modification.apply(text);
 	}
 	return text;
 }
